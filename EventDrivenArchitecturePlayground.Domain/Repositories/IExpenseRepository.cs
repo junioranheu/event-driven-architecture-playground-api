fa327a52185f99d408b544f77163842157367a64
junioranheu <junioranheu@gmail.com>
2026-07-11 16:41:50 -0300

    add domain

--- EventDrivenArchitecturePlayground.Domain/Repositories/IExpenseRepository.cs
+++ EventDrivenArchitecturePlayground.Domain/Repositories/IExpenseRepository.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EventDrivenArchitecturePlayground.Domain.Entities;
 
-namespace EventDrivenArchitecturePlayground.Domain.Repositories
+namespace EventDrivenArchitecturePlayground.Domain.Repositories;
+
+/// <summary>
+/// Por que a interface fica no Domain?
+/// O domínio define aquilo de que precisa.
+/// Mas ele não sabe como os dados serão persistidos.
+/// A implementação é criada na camada Infrastructure.
+/// Nesse caso, está sendo usado PostgreSQL e Entity Framework.
+/// Sendo assim, o domínio não depende de nenhuma tecnologia específica de persistência.
+/// </summary>
+public interface IExpenseRepository
 {
-    internal interface IExpenseRepository
-    {
-    }
+    Task AddAsync(
+        Expense expense,
+        CancellationToken cancellationToken = default);
+
+    Task<Expense?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyCollection<Expense>> GetByPeriodAsync(
+        DateTimeOffset startDate,
+        DateTimeOffset endDate,
+        int skip,
+        int take,
+        CancellationToken cancellationToken = default);
 }
