84834cfd03fcef5a089dfc17658509275521f45e
junioranheu <junioranheu@gmail.com>
2026-07-13 12:46:04 -0300

    fix comment

--- EventDrivenArchitecturePlayground.API/Program.cs
+++ EventDrivenArchitecturePlayground.API/Program.cs
@@ -16,7 +16,8 @@ WebApplication app = builder.Build();
 
 app.UsePresentation();
 
-// Em ambiente de desenvolvimento, inicie o projeto ConsoleConsumer para consumir mensagens do RabbitMQ.
+// Em ambiente de desenvolvimento, inicia-se o projeto ConsoleConsumer para consumir mensagens do RabbitMQ,
+// Simulando um outro serviço (real) que consome eventos publicados pela API.
 if (app.Environment.IsDevelopment())
 {
     Process.Start(new ProcessStartInfo
